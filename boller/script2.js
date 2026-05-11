68347e0b47fe84a28b84a97f04e1f1fb098a6084
markusmielnik01-jpg <markus.mielnik01@gmail.com>
2026-05-11 21:12:30 +0200

    gjort ferdig landing page, endret linker pga flytting inn i mapper, satt opp starter-ish på bollesiden og begynt litt på cookiesiden

--- boller/script2.js
+++ boller/script2.js
@@ -2,11 +2,13 @@ const trekkspillEl = document.querySelector(".trekkspill-box")
 const xEl = document.querySelector(".x")
 const burgerEl = document.querySelector(".burger")
 const ulEl = document.querySelector("ul")
-
+const logoEl = document.querySelector(".logo")
 
 /* nav-trekkspill */
 trekkspillEl.addEventListener("click", function(){
     xEl.classList.toggle("active")
     burgerEl.classList.toggle("active")
     ulEl.classList.toggle("active")
+    logoEl.classList.toggle("active")
+    trekkspillEl.classList.toggle("active")
 })
