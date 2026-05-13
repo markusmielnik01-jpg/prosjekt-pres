7a8299ee0438c96725a19b1901ac36b690d4040f
markusmielnik01-jpg <markus.mielnik01@gmail.com>
2026-05-13 21:35:08 +0200

    gjort ferdig cookie-siden

--- cookies/script1.js
+++ cookies/script1.js
@@ -5,10 +5,30 @@ const ulEl = document.querySelector("ul")
 const logoEl = document.querySelector(".logo")
 
 /* nav-trekkspill */
-trekkspillEl.addEventListener("click", function(){
+trekkspillEl.addEventListener("click", function () {
     xEl.classList.toggle("active")
     burgerEl.classList.toggle("active")
     ulEl.classList.toggle("active")
     logoEl.classList.toggle("active")
     trekkspillEl.classList.toggle("active")
+})
+
+
+
+
+const porsjonInputEl = document.getElementById("porsjonInput")
+
+
+porsjonInputEl.addEventListener("input", function () {
+    let porsjoner = porsjonInputEl.value
+
+    document.querySelector(".btr").innerHTML = porsjoner * 150
+    document.querySelector(".sgr").innerHTML = porsjoner * 150 
+    document.querySelector(".bsgr").innerHTML = porsjoner * 100
+    document.querySelector(".egg").innerHTML = porsjoner * 1
+    document.querySelector(".vsgr").innerHTML = porsjoner * 1
+    document.querySelector(".flr").innerHTML = porsjoner * 250 
+    document.querySelector(".ntr").innerHTML = porsjoner * 0.5
+    document.querySelector(".bp").innerHTML = porsjoner * 0.5
+    document.querySelector(".sb").innerHTML = porsjoner * 150
 })
