84e2f2a15afc209a3efb5133bd64e595c68caa2d
markusmielnik01-jpg <markus.mielnik01@gmail.com>
2026-05-26 21:21:48 +0200

    laget ferdig hele cookieclicker siden, og sørget for at alt stemmer overens i de ulike dokumentene

--- boller/script2.js
+++ boller/script2.js
@@ -5,10 +5,62 @@ const ulEl = document.querySelector("ul")
 const logoEl = document.querySelector(".logo")
 
 /* nav-trekkspill */
-trekkspillEl.addEventListener("click", function(){
+trekkspillEl.addEventListener("click", function () {
     xEl.classList.toggle("active")
     burgerEl.classList.toggle("active")
     ulEl.classList.toggle("active")
     logoEl.classList.toggle("active")
     trekkspillEl.classList.toggle("active")
+})
+
+
+const shopEl = document.querySelector("#shop")
+const counterEl = document.querySelector("#antallCookies")
+const cookieEl = document.querySelector(".cookie-click")
+const upgradeBtn = document.querySelector("#oppgrader")
+const gameboxEl = document.querySelector(".gamebox")
+const shopboxEl = document.querySelector(".shopbox")
+const prisEl = document.querySelector("#pris")
+const closeShopEl = document.querySelector(".x-shop")
+const infoEl = document.querySelector(".info")
+const currentEl = document.querySelector(".current")
+
+const upgrade = 2
+let rate = 1
+let antallCookies = 0
+let cost = 100
+
+currentEl.innerHTML = rate
+
+cookieEl.addEventListener("click", function () {
+    antallCookies = antallCookies + 1 * rate
+    counterEl.innerHTML = antallCookies
+})
+
+shopEl.addEventListener("click", function () {
+    gameboxEl.classList.toggle("active")
+    shopboxEl.classList.toggle("active")
+    infoEl.classList.toggle("active")
+})
+
+closeShopEl.addEventListener("click", function () {
+    gameboxEl.classList.toggle("active")
+    shopboxEl.classList.toggle("active")
+    infoEl.classList.toggle("active")
+
+
+})
+
+upgradeBtn.addEventListener("click", function () {
+    if (antallCookies < cost) {
+        alert("Du har ikke nok cookies!")
+    }
+
+    else{
+        rate = rate * 2
+        currentEl.innerHTML = rate
+        antallCookies = antallCookies - cost
+        cost = cost * 1.5
+        prisEl.innerHTML = Math.floor(cost)
+    }
 })
